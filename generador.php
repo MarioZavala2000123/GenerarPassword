e5198df5f6cb61418452a689fbbbcc9a74db0b6c
MarioZ26 <maritor2609@gmail.com>
2023-06-08 23:01:17 -0600

    mostrar dos tipos de estados

--- generador.php
+++ generador.php
@@ -12,21 +12,22 @@ function generarContraseña($longitud = 12) {
     return $contraseña;
 }
 
-// Verificar si se ha enviado la longitud 
-//$longitud = isset($_GET['longitud']) ? intval($_GET['longitud']) : 12;
-
 // Verificar la solicitud HTTP
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+} elseif ($contraseña === false) {
+//Verificar si tiene contraseña almacenada
+    http_response_code(500);
+    echo json_encode(array('error' => 'No se pudo generar la contraseña.'));
+} elseif ($contraseña === true) {
+//Si la contraseña esta llena realiza el proceso
     $longitud = isset($_GET['longitud']) ? $_GET['longitud'] : 12;
-    
     // Generar una contraseña con la longitud
     $contraseñaSegura = generarContraseña($longitud);
-
     // Devolver la contraseña generada como respuesta JSON
     header('Content-Type: application/json');
     echo json_encode(['Password' => $contraseñaSegura]);
 } else {
-    // Devolver un error en caso de que el método de solicitud no sea GET
+// Devolver un error en caso de que el método de solicitud no sea GET
     header('Content-Type: application/json');
     http_response_code(405);
     echo json_encode(['error' => 'Método no permitido']);
