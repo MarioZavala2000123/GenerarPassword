00c01e4ed4ad13d2d1a210e1672fe989ec27636f
MarioZ26 <maritor2609@gmail.com>
2023-06-10 07:12:58 -0600

    correccion error de logica en solicitudes

--- generador.php
+++ generador.php
@@ -1,5 +1,5 @@
 <?php
-//Función para generar contaseñas
+// Función para generar contraseñas
 function generarContraseña($longitud = 12) {
     $caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*';
     $contraseña = '';
@@ -14,20 +14,23 @@ function generarContraseña($longitud = 12) {
 
 // Verificar la solicitud HTTP
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-} elseif ($contraseña === false) {
-//Verificar si tiene contraseña almacenada
-    http_response_code(500);
-    echo json_encode(array('error' => 'No se pudo generar la contraseña.'));
-} elseif ($contraseña === true) {
-//Si la contraseña esta llena realiza el proceso
     $longitud = isset($_GET['longitud']) ? $_GET['longitud'] : 12;
-    // Generar una contraseña con la longitud
-    $contraseñaSegura = generarContraseña($longitud);
-    // Devolver la contraseña generada como respuesta JSON
-    header('Content-Type: application/json');
-    echo json_encode(['Password' => $contraseñaSegura]);
+
+    if ($longitud === '') {
+        // Verificar si tiene contraseña almacenada
+        http_response_code(500);
+        echo json_encode(array('error' => 'No se pudo generar la contraseña.'));
+    } else {
+        // Si la contraseña está llena, realiza el proceso
+
+        // Generar una contraseña con la longitud
+        $contraseñaSegura = generarContraseña($longitud);
+        // Devolver la contraseña generada como respuesta JSON
+        header('Content-Type: application/json');
+        echo json_encode(['Password' => $contraseñaSegura]);
+    }
 } else {
-// Devolver un error en caso de que el método de solicitud no sea GET
+    // Devolver un error en caso de que el método de solicitud no sea GET
     header('Content-Type: application/json');
     http_response_code(405);
     echo json_encode(['error' => 'Método no permitido']);
